5063f84fff9154fefe6252410635e113ebcafd00
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-10 21:17:32 -0700

    BigO challenge 2

---
+++ bigo/challenge33.js
@@ -0,0 +1,21 @@
+"use strict";
+
+// What is the Big O of the below function? (Hint, you may want to go line by line)
+function anotherFunChallenge(input) {
+    let a = 5;  // O(1)
+    let b = 10; // O(1)
+    let c = 50; // O(1)
+    for (let i = 0; i < input; i++) { // O(n)
+      let x = i + 1; // O(n)
+      let y = i + 2; // O(n)
+      let z = i + 3; // 0(n)
+    }
+
+    for (let j = 0; j < input; j++) { // O(n)
+      let p = j * 2; // O(n)
+      let q = j * 2; // O(n)
+    }
+    let whoAmI = "I don't know"; // O(1)
+  }
+
+  // BIGO is 4 * O(1) + 5 * O(n) => 9*O(n) => O(n)
