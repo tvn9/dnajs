a3eee7a4011165d6fa38488ab3d59a1ce0ec65d3
Thanh Nguyen <tv.nguyen9@icloud.com@users.noreply.github.com>
2022-08-24 23:47:12 -0700

    Add maxrepeatchar.js

---
+++ maxrepeatchar/maxrepeatchar.js
@@ -0,0 +1,28 @@
+// Find max repeated character in a string
+
+// Given string
+const fullName = "Thanh Vu Nguyen"
+const stringMap = {};
+
+
+
+
+
+
+
+function maxRepChar(str) {
+  // Iterate through the string
+  for (char of fullName) {
+    // insert the letter in to a new object 
+    if (!stringMap[char]) {
+      stringMap[char] = 1;
+    } else {
+      // increment the repeated letters count
+      stringMap[char] += 1;
+    }
+  }
+  // Return and new object 
+  return stringMap;
+}
+
+console.log(maxRepChar(fullName));
